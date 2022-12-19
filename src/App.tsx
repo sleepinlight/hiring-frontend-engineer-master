9820ee14a9e60196495175ea9b76c05302dea92f
Andy Carter <acarter264@gmail.com>
2022-12-19 07:56:14 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PayloadCard from "./components/PayloadCard";
+
+function App() {
+  return (
+    <div className="mx-10">
+      <PayloadCard />
+    </div>
+  );
+}
+
+export default App;
